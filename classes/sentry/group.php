14fa204598ca863d2877b5474952d6a893ac3900
Dan Harper <intouch@danharper.me>
2012-01-10 15:33:50 +0000

    Corrected join on Group users(). Should be on `user_id`, not `group_id`.

--- classes/sentry/group.php
+++ classes/sentry/group.php
@@ -217,7 +217,7 @@ class Sentry_Group
 			->from($users_table)
 			->where(static::$join_table.'.group_id', '=', $this->group['id'])
 			->join(static::$join_table)
-			->on(static::$join_table.'.group_id', '=', $users_table.'.id')
+			->on(static::$join_table.'.user_id', '=', $users_table.'.id')
 			->execute()->as_array();
 
 		if (count($users) == 0)
