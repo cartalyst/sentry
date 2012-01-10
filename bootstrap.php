4b42be204137bea07330e5cee4a7dd4469929ff6
Frank de Jonge <info@frenky.net>
2012-01-10 21:49:43 +1100

    Cleaned up bootstrap.

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Part of the Sentry package for Fuel.
+ * Part of the Sentry package for FuelPHP.
  *
  * @package    Sentry
  * @version    1.0
@@ -14,20 +14,20 @@
 Autoloader::add_core_namespace('Sentry');
 
 Autoloader::add_classes(array(
-	'Sentry\\Sentry' => __DIR__.'/classes/sentry.php',
-	'Sentry\\SentryAuthException' => __DIR__.'/classes/sentry.php',
+	'Sentry\\Sentry'                              => __DIR__.'/classes/sentry.php',
+	'Sentry\\SentryAuthException'                 => __DIR__.'/classes/sentry.php',
 	'Sentry\\SentryAuthUserNotActivatedException' => __DIR__.'/classes/sentry.php',
-	'Sentry\\SentryAuthConfigException' => __DIR__.'/classes/sentry.php',
+	'Sentry\\SentryAuthConfigException'           => __DIR__.'/classes/sentry.php',
 
-	'Sentry\\Sentry_Attempts' => __DIR__.'/classes/sentry/attempts.php',
-	'Sentry\\SentryAttemptsException' => __DIR__.'/classes/sentry/attempts/php',
-	'Sentry\\SentryUserSuspendedException' => __DIR__.'/classes/sentry/attempts.php',
+	'Sentry\\Sentry_Attempts'                     => __DIR__.'/classes/sentry/attempts.php',
+	'Sentry\\SentryAttemptsException'             => __DIR__.'/classes/sentry/attempts/php',
+	'Sentry\\SentryUserSuspendedException'        => __DIR__.'/classes/sentry/attempts.php',
 
-	'Sentry\\Sentry_User' => __DIR__.'/classes/sentry/user.php',
-	'Sentry\\SentryUserException' => __DIR__.'/classes/sentry/user.php',
-	'Sentry\\SentryUserNotFoundException' => __DIR__.'/classes/sentry/user.php',
+	'Sentry\\Sentry_User'                         => __DIR__.'/classes/sentry/user.php',
+	'Sentry\\SentryUserException'                 => __DIR__.'/classes/sentry/user.php',
+	'Sentry\\SentryUserNotFoundException'         => __DIR__.'/classes/sentry/user.php',
 
-	'Sentry\\Sentry_Group' => __DIR__.'/classes/sentry/group.php',
-	'Sentry\\SentryGroupException' => __DIR__.'/classes/sentry/group.php',
-	'Sentry\\SentryGroupNotFoundException' => __DIR__.'/classes/sentry/group.php',
+	'Sentry\\Sentry_Group'                        => __DIR__.'/classes/sentry/group.php',
+	'Sentry\\SentryGroupException'                => __DIR__.'/classes/sentry/group.php',
+	'Sentry\\SentryGroupNotFoundException'        => __DIR__.'/classes/sentry/group.php',
 ));
