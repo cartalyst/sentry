ebf32c2f33afd72915c24079b3ee4c0926177d85
iploppable <nandav1992@outlook.com>
2013-09-16 07:51:40 +0530

    Fixed docblock spacing (WhirlpoolHasher.php)

--- src/Cartalyst/Sentry/Hashing/WhirlpoolHasher.php
+++ src/Cartalyst/Sentry/Hashing/WhirlpoolHasher.php
@@ -1,38 +1,38 @@
 <?php namespace Cartalyst\Sentry\Hashing;
 /**
-* Part of the Sentry package.
-*
-* NOTICE OF LICENSE
-*
-* Licensed under the 3-clause BSD License.
-*
-* This source file is subject to the 3-clause BSD License that is
-* bundled with this package in the LICENSE file.  It is also available at
-* the following URL: http://www.opensource.org/licenses/BSD-3-Clause
-*
-* @package    Sentry
-* @version    2.0.0
-* @author     Cartalyst LLC
-* @license    BSD License (3-clause)
-* @copyright  (c) 2011 - 2013, Cartalyst LLC
-* @link       http://cartalyst.com
-*/
+ * Part of the Sentry package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Sentry
+ * @version    2.0.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011 - 2013, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
 
 class WhirlpoolHasher implements HasherInterface {
 
     /**
-    * Salt Length
-    *
-    * @var int
-    */
+     * Salt Length
+     *
+     * @var int
+     */
     public $saltLength = 16;
 
     /**
-    * Hash string.
-    *
-    * @param  string  $string
-    * @return string
-    */
+     * Hash string.
+     *
+     * @param  string  $string
+     * @return string
+     */
     public function hash($string)
     {
         // Create salt
@@ -42,12 +42,12 @@ class WhirlpoolHasher implements HasherInterface {
     }
 
     /**
-    * Check string against hashed string.
-    *
-    * @param  string  $string
-    * @param  string  $hashedString
-    * @return bool
-    */
+     * Check string against hashed string.
+     *
+     * @param  string  $string
+     * @param  string  $hashedString
+     * @return bool
+     */
     public function checkhash($string, $hashedString)
     {
         $salt = substr($hashedString, 0, $this->saltLength);
@@ -56,10 +56,10 @@ class WhirlpoolHasher implements HasherInterface {
     }
 
     /**
-    * Create a random string for a salt.
-    *
-    * @return string
-    */
+     * Create a random string for a salt.
+     *
+     * @return string
+     */
     public function createSalt()
     {
         $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
