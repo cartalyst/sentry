f74fc26cf34d81859d34276fe6aa2fd9b5ea354e
Ben Corlett <bencorlett@me.com>
2014-01-07 20:06:53 +1100

    Refactoring. Still a bit needed yet but it's making abstracting 3rd party ORMs easier.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- src/Cartalyst/Sentry/Users/IlluminateUserRepository.php
+++ src/Cartalyst/Sentry/Users/IlluminateUserRepository.php
@@ -19,41 +19,13 @@
  */
 
 use Carbon\Carbon;
-use Cartalyst\Sentry\Hashing\HasherInterface;
-use Closure;
 
-class IlluminateUserRepository implements UserRepositoryInterface {
+class IlluminateUserRepository extends BaseUserRepository implements UserRepositoryInterface {
 
 	/**
-	 * Hasher.
-	 *
-	 * @var \Cartalyst\Sentry\Hashing\HasherInterface
-	 */
-	protected $hasher;
-
-	/**
-	 * Model name.
-	 *
-	 * @var string
+	 * {@inheritDoc}
 	 */
 	protected $model = 'Cartalyst\Sentry\Users\EloquentUser';
-
-	/**
-	 * Create a new Illuminate user repository.
-	 *
-	 * @param  \Cartalyst\Sentry\Hashing\HasherInterface  $hasher
-	 * @param  string  $model
-	 * @return void
-	 */
-	public function __construct(HasherInterface $hasher, $model = null)
-	{
-		$this->hasher = $hasher;
-
-		if (isset($model))
-		{
-			$this->model = $model;
-		}
-	}
 
 	/**
 	 * {@inheritDoc}
@@ -97,19 +69,6 @@ class IlluminateUserRepository implements UserRepositoryInterface {
 		}
 
 		return $query->first();
-
-		if ($user and $this->validateCredentials($user, $password))
-		{
-			return $user;
-		}
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function validateCredentials(UserInterface $user, array $credentials)
-	{
-		return $this->hasher->check($credentials['password'], $user->password);
 	}
 
 	/**
@@ -148,119 +107,6 @@ class IlluminateUserRepository implements UserRepositoryInterface {
 	}
 
 	/**
-	 * {@inheritDoc}
-	 */
-	public function recordLogout(UserInterface $user)
-	{
-		return $user->save();
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function validForCreation(array $credentials)
-	{
-		return $this->validateUser($credentials);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function validForUpdate($user, array $credentials)
-	{
-		if ($user instanceof UserInterface)
-		{
-			$user = $user->getUserId();
-		}
-
-		return $this->validateUser($credentials, $user);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function create(array $credentials, Closure $callback = null)
-	{
-		$user = $this->createModel();
-		$this->fill($user, $credentials);
-
-		if ($callback)
-		{
-			$result = $callback($user);
-
-			if ($result === false)
-			{
-				return false;
-			}
-		}
-
-		$user->save();
-
-		return $user;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function update($user, array $credentials)
-	{
-		if ( ! $user instanceof UserInterface)
-		{
-			$user = $this->findById($user);
-		}
-
-		$this->fill($user, $credentials);
-		$user->save();
-
-		return $user;
-	}
-
-	/**
-	 * Parses the given credentials to return logins, password and others.
-	 *
-	 * @param  array  $credentials
-	 * @param  array  $loginNames
-	 * @return array
-	 * @throws \InvalidArgumentException
-	 */
-	protected function parseCredentials(array $credentials, array $loginNames)
-	{
-		if (isset($credentials['password']))
-		{
-			$password = $credentials['password'];
-			unset($credentials['password']);
-		}
-		else
-		{
-			$password = null;
-		}
-
-		$passedNames = array_intersect_key($credentials, array_flip($loginNames));
-
-		if (count($passedNames) > 0)
-		{
-			$logins = array();
-
-			foreach ($passedNames as $name => $value)
-			{
-				$logins[$name] = $credentials[$name];
-				unset($credentials[$name]);
-			}
-		}
-		elseif (isset($credentials['login']))
-		{
-			$logins = $credentials['login'];
-			unset($credentials['login']);
-		}
-		else
-		{
-			$logins = array();
-		}
-
-		return array($logins, $password, $credentials);
-	}
-
-	/**
 	 * Fills a user with the given credentials, intelligently.
 	 *
 	 * @param  \Cartalyst\Sentry\Users\UserInterface  $user
@@ -292,65 +138,6 @@ class IlluminateUserRepository implements UserRepositoryInterface {
 			$password = $this->hasher->hash($password);
 			$user->fill(compact('password'));
 		}
-	}
-
-	/**
-	 * Validates the user.
-	 *
-	 * @param  array  $credentials
-	 * @param  int  $id
-	 * @return bool
-	 * @throws \InvalidArgumentException
-	 */
-	protected function validateUser(array $credentials, $id = null)
-	{
-		$instance = $this->createModel();
-		$loginNames = $instance->getLoginNames();
-
-		// We will simply parse credentials which checks logins and passwords
-		list($logins, $password, $credentials) = $this->parseCredentials($credentials, $loginNames);
-
-		if ($id === null)
-		{
-			if (empty($logins))
-			{
-				throw new \InvalidArgumentException('No [login] credential was passed.');
-			}
-			if ($password === null)
-			{
-				throw new \InvalidArgumentException('You have not passed a [password].');
-			}
-		}
-
-		if ($password and strlen($password) < 6)
-		{
-			throw new \InvalidArgumentException('Your [password] must be at least 6 characters.');
-		}
-
-		return true;
-	}
-
-	/**
-	 * Create a new instance of the model.
-	 *
-	 * @return \Illuminate\Database\Eloquent\Model
-	 */
-	public function createModel()
-	{
-		$class = '\\'.ltrim($this->model, '\\');
-
-		return new $class;
-	}
-
-	/**
-	 * Runtime override of the model.
-	 *
-	 * @param  string  $model
-	 * @return void
-	 */
-	public function setModel($model)
-	{
-		$this->model = $model;
 	}
 
 }
