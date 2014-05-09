eedba6403d48178482d67f8f69a3e7f029eec47b
Peter Bex <peter@codeyellow.nl>
2014-05-09 10:44:28 +0200

    When adding or removing groups on a user object, the merged permissions cache should be cleared.

--- src/Cartalyst/Sentry/Users/Eloquent/User.php
+++ src/Cartalyst/Sentry/Users/Eloquent/User.php
@@ -493,6 +493,22 @@ class User extends Model implements UserInterface {
 		return $this->userGroups;
 	}
 
+    /**
+     * Clear the cached permissions attribute.
+     */
+    public function invalidateMergedPermissionsCache()
+    {
+		$this->mergedPermissions = null;
+    }
+
+    /**
+     * Clear the cached user groups attribute.
+     */
+    public function invalidateUserGroupsCache()
+    {
+		$this->userGroups = null;
+    }
+    
 	/**
 	 * Adds the user to the given group.
 	 *
@@ -504,7 +520,8 @@ class User extends Model implements UserInterface {
 		if ( ! $this->inGroup($group))
 		{
 			$this->groups()->attach($group);
-			$this->userGroups = null;
+			$this->invalidateUserGroupsCache();
+			$this->invalidateMergedPermissionsCache();
 		}
 
 		return true;
@@ -521,7 +538,8 @@ class User extends Model implements UserInterface {
 		if ($this->inGroup($group))
 		{
 			$this->groups()->detach($group);
-			$this->userGroups = null;
+			$this->invalidateUserGroupsCache();
+			$this->invalidateMergedPermissionsCache();
 		}
 
 		return true;
--- tests/EloquentUserTest.php
+++ tests/EloquentUserTest.php
@@ -110,9 +110,11 @@ class EloquentUserTest extends PHPUnit_Framework_TestCase {
 		$relationship = m::mock('StdClass');
 		$relationship->shouldReceive('attach')->with($group)->once();
 
-		$user  = m::mock('Cartalyst\Sentry\Users\Eloquent\User[inGroup,groups]');
+		$user  = m::mock('Cartalyst\Sentry\Users\Eloquent\User[inGroup,groups,invalidateMergedPermissionsCache,invalidateUserGroupsCache]');
 		$user->shouldReceive('inGroup')->once()->andReturn(false);
 		$user->shouldReceive('groups')->once()->andReturn($relationship);
+		$user->shouldReceive('invalidateUserGroupsCache')->once();
+		$user->shouldReceive('invalidateMergedPermissionsCache')->once();
 
 		$this->assertTrue($user->addGroup($group));
 	}
@@ -124,9 +126,11 @@ class EloquentUserTest extends PHPUnit_Framework_TestCase {
 		$relationship = m::mock('StdClass');
 		$relationship->shouldReceive('detach')->with($group)->once();
 
-		$user  = m::mock('Cartalyst\Sentry\Users\Eloquent\User[inGroup,groups]');
+		$user  = m::mock('Cartalyst\Sentry\Users\Eloquent\User[inGroup,groups,invalidateMergedPermissionsCache,invalidateUserGroupsCache]');
 		$user->shouldReceive('inGroup')->once()->andReturn(true);
 		$user->shouldReceive('groups')->once()->andReturn($relationship);
+		$user->shouldReceive('invalidateUserGroupsCache')->once();
+		$user->shouldReceive('invalidateMergedPermissionsCache')->once();
 
 		$this->assertTrue($user->removeGroup($group));
 	}
