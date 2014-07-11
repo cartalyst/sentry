7802e4bf998019a10452f351e78dffe50d285760
halfik <halfik@gmail.com>
2014-07-11 13:21:50 +0200

    auth social abstract

---
+++ src/Cartalyst/Sentry/Auth/Providers/SocialAbstract.php
@@ -0,0 +1,18 @@
+<?php namespace Cartalyst\Sentry\Auth\Providers;
+
+
+abstract class SocialAbstract implements AuthProviderInferface {
+    /**
+     * metoda do autoryzacji
+     * @param array $credentials
+     * @param bool $remember
+     * @return mixed
+     */
+    public abstract function authorize(array $credentials, $remember=false);
+
+    /**
+     * rejestracja uzytkownika w systemie
+     * @return mixed
+     */
+    public abstract function register();
+}
