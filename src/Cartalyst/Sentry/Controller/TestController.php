e20416943dc86cf352fed6da9e5cc4d38aa03302
halfik <halfik@gmail.com>
2014-07-15 16:08:43 +0200

    xx

---
+++ src/Cartalyst/Sentry/Controller/TestController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cartalyst\Sentry\Controllers;
+
+class TestController extends Controller {
+    public function test(){
+        echo 23; exit;
+    }
+}
---
+++ src/Cartalyst/Sentry/routes.php
@@ -0,0 +1,2 @@
+<?php
+Route::get('/test', 'Cartalyst\Sentry\Controllers@test');
