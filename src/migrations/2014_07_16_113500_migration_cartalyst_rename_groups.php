3d730da57ed04e14f9e0aa542019eab4d8bcc513
halfik <halfik@gmail.com>
2014-07-16 12:06:13 +0200

    fix query do migracji dla acl

--- src/migrations/2014_07_16_113500_migration_cartalyst_rename_groups.php
+++ src/migrations/2014_07_16_113500_migration_cartalyst_rename_groups.php
@@ -11,8 +11,8 @@ class MigrationCartalystRenameGroups extends Migration {
     public function up()
     {
         Schema::rename('groups', 'roles');
-        DB::query('ALTER TABLE "roles" ALTER COLUMN "created_at" SET DEFAULT now();');
-        DB::query('ALTER TABLE "roles" ALTER COLUMN "updated_at" SET DEFAULT now();');
+        DB::raw('ALTER TABLE "roles" ALTER COLUMN "created_at" SET DEFAULT now();');
+        DB::raw('ALTER TABLE "roles" ALTER COLUMN "updated_at" SET DEFAULT now();');
     }
 
     /**
@@ -23,8 +23,8 @@ class MigrationCartalystRenameGroups extends Migration {
     public function down()
     {
         Schema::rename('roles', 'groups');
-        DB::query('ALTER TABLE "groups" ALTER COLUMN "created_at" DROP DEFAULT;');
-        DB::query('ALTER TABLE "groups" ALTER COLUMN "updated_at" DROP DEFAULT;');
+        DB::raw('ALTER TABLE "groups" ALTER COLUMN "created_at" DROP DEFAULT;');
+        DB::raw('ALTER TABLE "groups" ALTER COLUMN "updated_at" DROP DEFAULT;');
     }
 
 }
