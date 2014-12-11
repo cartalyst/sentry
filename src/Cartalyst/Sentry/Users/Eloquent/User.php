840e2bc8f61c6110f3bcc14ef713f336937c67d4
Jamie Kahgee <jkahgee@ine.com>
2014-12-11 11:43:31 -0500

    fix method call capitalization to match interface methods

--- src/Cartalyst/Sentry/Users/Eloquent/User.php
+++ src/Cartalyst/Sentry/Users/Eloquent/User.php
@@ -806,7 +806,7 @@ class User extends Model implements UserInterface {
 			throw new \RuntimeException("A hasher has not been provided for the user.");
 		}
 
-		return static::$hasher->checkHash($string, $hashedString);
+		return static::$hasher->checkhash($string, $hashedString);
 	}
 
 	/**
